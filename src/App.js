08567b818377c63c023a93ad5561f91c2da03309
Sumit Jangir <sumitjangir.jecrc@gmail.com>
2020-08-02 22:46:43 +0530

    html and css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img
+            alt="logo"
+            className="App-logo"
+            src={process.env.PUBLIC_URL + "/images/logo512.png"}
+          />
+        </header>
+        <div className="tweet-textbox">
+          <textarea
+            class="form-control"
+            id="tweet-text"
+            rows="3"
+            placeholder="What's happening?"
+          ></textarea>
+        </div>
+        <div className="text-right">
+          <button type="button" class="btn btn-info tweet-btn">
+            Tweet
+          </button>
+        </div>
+        <div className="grey-v-gap"></div>
+        <footer></footer>
+      </div>
+    );
+  }
 }
 
 export default App;
