4f83a5789347f551be0c7f2ddc722bb35965bd9e
Sumit Jangir <sumitjangir.jecrc@gmail.com>
2020-08-03 17:45:59 +0530

    css changes

--- src/App.js
+++ src/App.js
@@ -31,22 +31,46 @@ class App extends Component {
         style={{ margin: "auto" }}
       >
         <div className="col-md-3">
-          <div className="d-flex align-items-start flex-column justify-content-start">
-            <a href="#" className="p-2 btn btn-sm btn-block text-left">
+          <div className="w-100 align-top">
+            <div className="w-100">
+              <img
+                alt="logo"
+                className="App-logo"
+                src={process.env.PUBLIC_URL + "/images/logo512.png"}
+              />
+            </div>
+            <a href="#" className="btn btn-sm btn-block text-left rounded-pill">
               Home
             </a>
-            <a href="#" className="p-2 btn  btn-sm btn-block text-left">
+            <a
+              href="#"
+              className="btn  btn-sm btn-block text-left rounded-pill"
+            >
               Explore
+            </a>
+            <a
+              href="#"
+              className="btn  btn-sm btn-block text-left rounded-pill"
+            >
+              Notifications
+            </a>
+            <a
+              href="#"
+              className="btn  btn-sm btn-block text-left rounded-pill"
+            >
+              Messages
+            </a>
+            <a
+              href="#"
+              className="btn  btn-sm btn-block text-left rounded-pill"
+            >
+              Profile
             </a>
           </div>
         </div>
         <div className="col-md-6 align-self-stretch border-left border-right padding-0">
           <header className="App-header">
-            <img
-              alt="logo"
-              className="App-logo"
-              src={process.env.PUBLIC_URL + "/images/logo512.png"}
-            />
+            <p className="font-weight-bold">Home</p>
           </header>
           <div className="tweet-textbox">
             <textarea
